c62535b50bc6adaf5a5942b3cb20e047deb06dd4
Gita <gita@srlabs.id>
2023-01-13 13:44:59 +0700

    React: Slideshow App solution

--- src/components/Slides.js
+++ src/components/Slides.js
@@ -1,17 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function Slides({slides}) {
-
+    const [count, setCount] = useState(0);
+    
     return (
         <div>
             <div id="navigation" className="text-center">
-                <button data-testid="button-restart" className="small outlined">Restart</button>
-                <button data-testid="button-prev" className="small">Prev</button>
-                <button data-testid="button-next" className="small">Next</button>
+                <button data-testid="button-restart" disabled={count === 0} className="small outlined" onClick={() => setCount(0)}>Restart</button>
+                <button data-testid="button-prev" disabled={count === 0} className="small" onClick={() => setCount(count-1)}>Prev</button>
+                <button data-testid="button-next" disabled={count === slides.length-1} className="small" onClick={() => setCount(count+1)}>Next</button>
             </div>
             <div id="slide" className="card text-center">
-                <h1 data-testid="title">Slide Title Here</h1>
-                <p data-testid="text">Slide Text Here</p>
+                <h1 data-testid="title">{slides[count].title}</h1>
+                <p data-testid="text">{slides[count].text}</p>
             </div>
         </div>
     );
